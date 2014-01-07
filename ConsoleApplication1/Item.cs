3cd8bf435be4bed252a9d97df3971c2279245ab6
Tomasz Mazurek <tmazurek@windowslive.com>
2014-01-07 07:50:02 +0100

    go

---
+++ ConsoleApplication1/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Warehouse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Warehouse
+    {
+        public int WarehouseID { get; set; }
+        public string WarehouseName { get; set; }
+
+        public Warehouse(string Name, int ID)
+        {
+            WarehouseName = Name;
+            WarehouseID = ID;
+        }
+        public Item FinAndReturnItem(int ItemID)
+        {
+            Item returnItem = new Item() { ID = ItemID, Name="micr office 2001" };
+            return returnItem;
+        }
+    }
+}

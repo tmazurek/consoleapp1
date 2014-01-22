6cd299ab5ca0d9aac16016b46e4a4b8801cd293e
Tomasz Mazurek <tmazurek@windowslive.com>
2014-01-22 14:16:46 +0100

    doitnow

---
+++ DataBindingTest/SoftwareItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataBindingTest
+{
+    public class SoftwareItem
+    {
+        public string Name { get; set; }
+        public int ISBN { get; set; }
+        public string Description { get; set; }
+
+        public static SoftwareItem GetItem()
+        {
+            var item1 = new SoftwareItem() {Name="Outlook", Description="Mail management tool", ISBN=12333};
+            return item1;
+        }
+    }
+}
